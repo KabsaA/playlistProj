65b0f773ebd7f47d694f7991ecd4b6c75a6620e2
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-10-01 15:26:57 -0700

    added mongodb and playlist files

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
-app = Flask(__name__)
-
-from flask import Flask, render_template
-
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+def playlists_index():
+    '''Show all the playlist the the user'''
+    return render_template('playlists_index.html', playlists=playlists.find())
+
+@app.route('/playlists/new')
+def playlist_new():
+    '''Show form for creating a new playlist'''
+    return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    '''Submit new playlist from the user'''
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
