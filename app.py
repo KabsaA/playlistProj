6830765673d68de987e3c289ed915e723ea7fb77
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-10-01 13:54:15 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template('home.html', msg='Flask is Cool!!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
